4202e228a08510c5a3db91cc136eb5d635700cc8
Prakhar Sankrityayan <prakhar3099@gmail.com>
2022-08-01 11:57:13 +0530

    Prakhar's inital commit

---
+++ src/main/java/com/prakhar/Prakhar.java
@@ -0,0 +1,11 @@
+package com.prakhar;
+
+public class Prakhar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi I am Prakhar");
+
+	}
+
+}
