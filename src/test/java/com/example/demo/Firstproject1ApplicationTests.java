414639657e4d9f28334daa00330a770dc8e61f7a
Kushagra <kushagraj81@gmail.com>
2022-08-01 11:41:16 +0530

    first commit

---
+++ src/test/java/com/example/demo/Firstproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Firstproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
